644d57957d089c2cd070e2355f4d469bb831dfe9
Roberto Piga <rpiga.net@gmail.com>
2017-01-28 21:47:35 +0100

    Add files via upload

---
+++ Pset1/pset1-1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 28 21:27:47 2017
+
+@author: Mork
+"""
+
+s = "azcbobobegghakl"
+
+count = 0
+for char in s:
+    if char in ('a', 'e', 'i', 'o', 'u'):
+        count += 1
+        
+print("Number of vowels:", count)
