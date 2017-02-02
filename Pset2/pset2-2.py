e39b88041ae5e0a2fa4ca5978e8e3da07beccd00
Roberto Piga <rpiga.net@gmail.com>
2017-02-02 21:48:03 +0100

    Update pset2-2.py

--- Pset2/pset2-2.py
+++ Pset2/pset2-2.py
@@ -6,25 +6,20 @@ Created on Thu Feb  2 00:25:26 2017
 """
 # Paste your code into this box
 # define variables like in the example, below
-# balance = 3329; annualInterestRate = 0.2
+balance = 3329; annualInterestRate = 0.2
 
 minimumFixed = 0
 f = True
 initialBalance = balance
-while f:
+
+while balance > 0:
+    balance = initialBalance
+    minimumFixed += 10
+    
     for month in range(1,13):
         unpaidBalance = balance - minimumFixed
         balance =  unpaidBalance + round(annualInterestRate/12.0 * unpaidBalance)
 
-    if balance < 0:
-        f = False
-    else:
-        minimumFixed += 10
-        balance = initialBalance
-    
-    
 print("Lowest Payment:", round(minimumFixed))
-
-
 
 
